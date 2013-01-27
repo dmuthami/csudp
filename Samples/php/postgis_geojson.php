b6077de1783c2b1ea0505f358e9b8c5802b29f20
dmuthami <waruid@gmail.com>
2013-01-27 22:58:30 +0300

    Version 1.0.0.3

--- Samples/php/postgis_geojson.php
+++ Samples/php/postgis_geojson.php
@@ -3,9 +3,9 @@
 /**
  * Title: PostGIS to GeoJSON
  * Notes: Query a PostGIS table or view and return the results in GeoJSON format, suitable for use in OpenLayers, Leaflet, etc.
- * Author: Bryan R. McBride, GISP
+ * Author: David Muthami
  * Contact: bryanmcbride.com
- * GitHub: https://github.com/bmcbride/PHP-Database-GeoJSON
+ * GitHub: url
  */
 # Connect to PostgreSQL database
 //$conn = new PDO('pgsql:host=localhost;dbname=cso;port=5432','dmuthami','Q4jqt3ee9H');
@@ -15,7 +15,7 @@ $conn = pg_connect("dbname='cso' host='localhost' port='5432' user='postgres' pa
 
 # Build SQL SELECT statement and return the geometry as a GeoJSON element
 //$sql = 'SELECT *, public.ST_AsGeoJSON(public.ST_Transform((the_geom),4326)) as geojson FROM csoinformation';
-$sql = 'SELECT *, public.ST_AsGeoJSON(public.ST_Transform((geom),4326)) as geojson FROM county';
+$sql = 'SELECT county, public.ST_AsGeoJSON(public.ST_Transform((geom),4326)) as geojson FROM county;';
 
 # Try query or error
 //$rs = $conn->query($sql);
@@ -27,30 +27,52 @@ if (!$rs) {
 }
 
 # Build GeoJSON feature collection array
-$geojson = array(
-    'type' => 'FeatureCollection',
-    'features' => array()
-);
 
-# Loop through rows to build feature arrays
-//$arr = pg_fetch_array($result, $row_counter, PGSQL_ASSOC)
-$row_counter = 0;
-while ($row = pg_fetch_array($rs, $row_counter, PGSQL_ASSOC)) {
-    $properties = $row;
-    # Remove geojson and geometry fields from properties
-    unset($properties['geojson']);
-    unset($properties['geom']);
-    $feature = array(
-        'type' => 'Feature',
-        'geometry' => json_decode($row['geojson'], true),
-        'properties' => $properties
+$geojson = build_geojson($rs);
+echo $geojson;
+
+//header('Content-type: application/json');
+//echo json_encode($geojson, JSON_NUMERIC_CHECK);
+// Close database connection
+pg_close($conn);
+$conn = NULL;
+
+/*
+ * Generates a GeoJSON file directly from PostgreSQL
+ */
+
+function build_geojson($rs) {
+    $geojson = array(
+        'type' => 'FeatureCollection',
+        'features' => array()
     );
-    # Add feature arrays to feature collection array
-    array_push($geojson['features'], $feature);
-    $row_counter++;
+    while ($row = pg_fetch_assoc($rs)) {
+
+        $properties = array();
+
+        foreach ($row as $key => $val) {
+
+            if ($key != 'geojson') {
+
+// array_push($properties,$key,$val);
+
+                $properties[$key] = $val;
+            };
+        };
+
+        $feature = array(
+            'type' => 'Feature',
+            'properties' => $properties,
+            'geometry' => json_decode($row['geojson'], true),
+        );
+
+// Add feature array to feature collection array
+
+        array_push($geojson['features'], $feature);
+    }
+//header('Content-type: application/json',true);
+
+    return json_encode($geojson);
 }
 
-header('Content-type: application/json');
-echo json_encode($geojson, JSON_NUMERIC_CHECK);
-$conn = NULL;
 ?>
